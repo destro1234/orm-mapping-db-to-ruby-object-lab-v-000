02af202ed1854c58082266ec7d4ebb707af3e98f
Elias Ventura <eliasventura23@gmail.com>
2019-01-18 21:44:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,9 +85,13 @@ class Student
 
   def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT ?
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, num)
 
 
   end
