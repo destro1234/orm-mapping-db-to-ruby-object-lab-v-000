95b33458e3989cd208505cf0ca28b8e72c83b8fb
Elias Ventura <eliasventura23@gmail.com>
2019-01-18 21:24:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,14 @@ class Student
     SQL
 
     DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE IF EXISTS students;
+    SQL
+
+    DB[:conn].execute(sql)
 
   end
 
