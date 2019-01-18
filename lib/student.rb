7568d22eeeb4bb605b7d4718719237a8dc20d72b
Elias Ventura <eliasventura23@gmail.com>
2019-01-18 21:39:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,10 @@ class Student
   end
 
   def self.students_below_12th_grade
-    sql <<-SQL
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < 12
     SQL
 
     DB[:conn].execute(sql)
